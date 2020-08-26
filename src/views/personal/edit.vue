06d27ae024e6f2e2b396764a6414c04d66aade09
laohaishi <1945350047@qq.com>
2020-08-26 16:13:19 +0800

    打包

--- src/views/personal/edit.vue
+++ src/views/personal/edit.vue
@@ -8,16 +8,24 @@
       alt
       class="avatar"
     />
-    <PersonalBar labelText='昵称' descText='火星网友' @handleClick="$router.push('/yemian1')" />
-    <PersonalBar labelText='密码' descText='******' @handleClick="$router.push('/yemian1')" />
-    <PersonalBar labelText='性别' descText='男' @handleClick="$router.push('/yemian1')" />
-   
+    <PersonalBar 
+        labelText="昵称" 
+        descText="火星网友" 
+        @handleClick="$router.push('/yemian1')" />
+    <PersonalBar 
+        labelText="密码" 
+        descText="******" 
+        @handleClick="$router.push('/yemian1')" />
+    <PersonalBar 
+        labelText="性别" 
+        descText="男" 
+        @handleClick="$router.push('/yemian1')" />
   </div>
 </template>
 
 <script>
 import Title from "../../components/title";
-import PersonalBar from '../../components/personalBar'
+import PersonalBar from "../../components/personalBar";
 export default {
   data() {
     return {
@@ -26,20 +34,18 @@ export default {
   },
   components: {
     Title,
-    PersonalBar
+    PersonalBar,
   },
 };
 </script>
 
 <style lang='less' scoped>
-img{
-    display: flex;
-    // align-items: center;
-    width: 70/360*100vw;
-    height: 70/360*100vw;
-    object-fit: cover;
-    border-radius: 50%;
-    margin: auto;
-    
+img {
+  display: flex;
+  width: 70/360 * 100vw;
+  height: 70/360 * 100vw;
+  object-fit: cover;
+  border-radius: 50%;
+  margin: auto;
 }
 </style>
